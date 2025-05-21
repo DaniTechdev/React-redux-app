215398bbc0e9d68ddfc611dbd05ac8cf033ea16e
unknown <tochidan11@gmail.com>
2025-05-21 08:39:43 +0100

    written my first redux function reducer of bug add slice with if and else statement

---
+++ redux-app/src/reducer.js
@@ -0,0 +1,23 @@
+//[]
+
+let lastId = 0;
+
+function reducer(state = [], action) {
+  //state is like the store state
+  if (action.type == "bugAdded") {
+    return [
+      ...state,
+      {
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
+      },
+    ];
+  } else if (action.type === "bugRemoved") {
+    return state.filter((bug) => bug.id !== action.payload.id);
+  }
+  //now if we dispatch action that is not in our system, then we should return the
+  //current state to avoid destroying our store states
+
+  return state;
+}
